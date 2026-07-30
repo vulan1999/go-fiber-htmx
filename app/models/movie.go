c47effe3575a9572bfd6daafef931922c50ab324
Vu Lan Nguyen <vulan1999@gmail.com>
2026-07-30 22:35:08 +0700

    fix error for NumberInt type in mongo return incorrect response

--- app/models/movie.go
+++ app/models/movie.go
@@ -1,11 +1,31 @@
 package models
 
-import "go.mongodb.org/mongo-driver/v2/bson"
+import (
+	"encoding/json"
+	"regexp"
 
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+// helper function to clean byte to get clean int number.
+// For Example: "year": "2012è" -> "year": "2012"
+type CleanInt string
+
+func (c CleanInt) MarshalJSON() ([]byte, error) {
+	// replace all rune that not number to empty string
+	reg := regexp.MustCompile("[^0-9]")
+	cleaned := reg.ReplaceAllString(string(c), "")
+	if cleaned == "" {
+		return json.Marshal(0)
+	}
+	return json.Marshal(cleaned)
+}
+
+// Define type
 type Movie struct {
 	ID     bson.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Title  string        `json:"title" bson:"title"`
 	Plot   string        `json:"plot" bson:"plot"`
 	Poster string        `json:"poster" bson:"poster"`
-	Year   string        `json:"year" bson:"year"`
+	Year   CleanInt      `json:"year" bson:"year"`
 }
